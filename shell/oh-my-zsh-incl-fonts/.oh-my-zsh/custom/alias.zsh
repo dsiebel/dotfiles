311eea7e90690e013d3ab9f492771f0b5614303c
Dominik Siebel <dsiebel@users.noreply.github.com>
2016-01-22 15:15:33 +0100

    Add php env switching aliases
    `brew unlink` and `link` different php environments/versions with a simple shortcut

--- shell/oh-my-zsh-incl-fonts/.oh-my-zsh/custom/alias.zsh
+++ shell/oh-my-zsh-incl-fonts/.oh-my-zsh/custom/alias.zsh
@@ -2,4 +2,10 @@ alias fuck='eval $(thefuck $(fc -ln -1 | tail -n 1)); fc -R'
 
 alias hcomposer='hhvm /usr/local/bin/composer'
 
+alias gds='git diff --staged'
+
 alias hosts='sudo vim /etc/hosts; dscacheutil -flushcache'
+
+alias use_php54="brew unlink php56 && brew unlink php70 && brew link php54"
+alias use_php56="brew unlink php54 && brew unlink php70 && brew link php56"
+alias use_php70="brew unlink php54 && brew unlink php56 && brew link php70"
