0df73cffde349267c59fba6d2c158e39d48058e4
dsiebel <code@dsiebel.de>
2016-11-30 14:09:32 +0100

    add aliases for composer and php debugging

--- shell/oh-my-zsh-incl-fonts/.oh-my-zsh/custom/alias.zsh
+++ shell/oh-my-zsh-incl-fonts/.oh-my-zsh/custom/alias.zsh
@@ -9,3 +9,8 @@ alias hosts='sudo vim /etc/hosts; dscacheutil -flushcache'
 
 alias use_php56="brew unlink php70 && brew link php56"
 alias use_php70="brew unlink php56 && brew link php70"
+
+# alias composer='/usr/local/bin/composer_noxdebug.sh'
+# alias composer='COMPOSER_DISABLE_XDEBUG_WARN=1 /usr/local/bin/php -d xdebug.remote_enable=0 -d xdebug.profiler_enable=0 -d xdebug.default_enable=0 /usr/local/bin/composer'
+alias composer='PHP_INI_SCAN_DIR=/usr/local/etc/php/composer/conf.d php -c /usr/local/etc/php/composer /usr/local/bin/composer'
+alias phpd='php -dxdebug.remote_enable=1 -dxdebug.remote_mode=req -dxdebug.remote_autostart=1 -dxdebug.remote_port=9000 -dxdebug.remote_host=127.0.0.1'
