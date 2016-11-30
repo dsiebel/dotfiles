d6632a2fe8925232bfa3b9d242fdd45f36f93bc1
dsiebel <dominik.siebel@trivago.com>
2016-11-30 14:06:27 +0100

    add shortcut alias `gbs` for `git branch -av`

--- shell/oh-my-zsh-incl-fonts/.oh-my-zsh/custom/alias.zsh
+++ shell/oh-my-zsh-incl-fonts/.oh-my-zsh/custom/alias.zsh
@@ -3,6 +3,7 @@ alias fuck='eval $(thefuck $(fc -ln -1 | tail -n 1)); fc -R'
 alias hcomposer='hhvm /usr/local/bin/composer'
 
 alias gds='git diff --staged'
+alias gbs='git branch -av'
 
 alias hosts='sudo vim /etc/hosts; dscacheutil -flushcache'
 
