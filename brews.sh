624d3478e19d1cbac39caf4336091509a30dc018
dsiebel <code@dsiebel.de>
2016-12-13 13:20:00 +0100

    tranfer brews.txt into executable brews.sh script

---
+++ brews.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+# install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+
+# Install zsh
+brew install zsh
+
+# Switch to using brew-installed zsh as default shell
+if ! fgrep -q '/usr/local/bin/zsh' /etc/shells; then
+  echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/bash;
+fi;
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install more recent versions of some macOS tools.
+brew install vim --override-system-vi
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install homebrew/dupes/screen
+
+# Install git tools
+brew install git
+brew install git-lfs
+
+# Install other usefull binaries
+brew install hugo
+brew install ngrep
+brew install xz
+brew install tree
+
+# Install php & extensions
+brew install php70
+brew install php70-apcu
+brew install php70-imagick
+brew install php70-intl
+brew install php70-mcrypt
+brew install php70-opcache
+brew install php70-xdebug
+# some extension are not considered "stable", yet
+brew install --HEAD php70-igbinary
+brew install --HEAD php70-memcached
+brew install --HEAD php70-redis
+
+# install composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php -r "unlink('composer-setup.php');"
+
+# allow non-https for composer
+composer config --global secure-http false
