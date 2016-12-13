3edcb1ba39f3f4f9c49cf5fa6e85b2d749c29760
dsiebel <dominik.siebel@trivago.com>
2016-12-13 14:52:23 +0100

    add casks install script

---
+++ casks.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Install all the applications
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install phpstorm
+brew cask install sublime-text
+brew cask install kaleidoscope
+brew cask install alfred
+brew cask install tunnelblick
+brew cask install vlc
+brew cask install spotify
+brew cask install sequel-pro
+brew cask install skype
+brew cask install docker
+brew cask install dash
+brew cask install ccmenu
+brew cask install sourcetree
+brew cask install 1password
+brew cask install carbon-copy-cloner
+brew cask install slack
+brew cask install logitech-options
+brew cask install dropbox
+brew cask install evernote
+brew cask install macdown
+brew cask install paw
+brew cask install vagrant
+brew cask install vagrant-manager
+brew cask install virtualbox
+brew cask install the-unarchiver
+brew cask install adobe-creative-cloud
