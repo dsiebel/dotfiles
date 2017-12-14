edcdd098ebb98978597bb95d65f478be36c24d53
dsiebel <code@dsiebel.de>
2017-12-14 10:36:56 +0100

    move composer install to separate script

--- brews.sh
+++ brews.sh
@@ -67,12 +67,3 @@ brew install php71-memcached
 brew install php71-opcache
 brew install php71-redis
 brew install php71-xdebug
-
-# install composer
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-php -r "unlink('composer-setup.php');"
-
-# allow non-https for composer
-composer config --global secure-http false
