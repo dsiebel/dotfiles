afd94f1cf73bf61d7eb5e94c14f1d5bf1077f508
dsiebel <dominik.siebel@trivago.com>
2017-12-14 14:38:35 +0100

    add script to set up docker-machine

---
+++ docker-machine.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+MACHINE="${1:-default}"
+
+echo "MACHINE: ${MACHINE}"
+
+# set up a better docker-machine
+if [[ "$(docker-machine status ${MACHINE})" == 0 ]]; then
+  docker-machine kill "${MACHINE}" && docker-machine rm "${MACHINE}"
+fi
+
+docker-machine create --driver "virtualbox" \
+ --virtualbox-cpu-count "-1" \
+ --virtualbox-disk-size "40000" \
+ --virtualbox-memory "4096" \
+ --virtualbox-no-share \
+ --virtualbox-host-dns-resolver \
+ "${MACHINE}"
