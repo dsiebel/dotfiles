219d0be0adc233ab33279b489a30ad04e82f9ac1
Dominik Siebel <code@dsiebel.de>
2018-06-22 10:52:51 +0200

    test all the things

--- bin/vgaswitcheroo.sh
+++ bin/vgaswitcheroo.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-MSG=$1
-
 if [[ "SHOW" = "$1" ]]; then
 	cat /sys/kernel/debug/vgaswitcheroo/switch
 else
 	if [[ "" != "$1" ]]; then
-		echo $1 > /sys/kernel/debug/vgaswitcheroo/switch
+		echo "$1" > /sys/kernel/debug/vgaswitcheroo/switch
 	else
 		exit 2
 	fi
