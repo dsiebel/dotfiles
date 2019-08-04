896a844cf872fedf1310f79b420e1f26df387027
Dominik Siebel <code+github@dsiebel.de>
2019-08-04 12:23:12 +0200

    cleanup, update casks

--- casks.sh
+++ casks.sh
@@ -1,41 +1,47 @@
 #!/usr/bin/env bash
 
 # Install all the applications
-brew cask install google-chrome
-brew cask install firefoxdeveloperedition
-brew cask install iterm2
-brew cask install jetbrains-toolbox
-brew cask install visual-studio-code
-brew cask install kaleidoscope
-brew cask install alfred
-brew cask install tunnelblick
-brew cask install vlc
-brew cask install spotify
-brew cask install sequel-pro
-brew cask install skype
-brew cask install docker-toolbox
-brew cask install docker
-brew cask install minikube
-brew cask install dash
-brew cask install ccmenu
-brew cask install sourcetree
 #brew cask install 1password ## need to get from AppStore
-#brew cask install carbon-copy-cloner ## no license currently
-brew cask install slack
-brew cask install logitech-options
-brew cask install dropbox
-brew cask install macdown
-brew cask install paw
-brew cask install vagrant
-brew cask install vagrant-manager
-brew cask install virtualbox
-brew cask install the-unarchiver
+brew cask install 1password-cli
 brew cask install adobe-creative-cloud
-brew cask install duet
-brew cask install gimp
-brew cask install bettertouchtool
-# brew cask install divvy ## need to get from AppStore
-brew cask install moom
 brew cask install alfred
 # Still have to play around with it...
 # brew cask install amethyst
+brew cask install bettertouchtool
+#brew cask install carbon-copy-cloner ## no license currently
+brew cask install ccmenu
+brew cask install dash
+# brew cask install divvy ## need to get from AppStore
+brew cask install docker
+brew cask install docker-toolbox
+brew cask install dropbox
+brew cask install duet
+brew cask install firefox firefox-developer-edition
+brew cask install gimp
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install istat-menus
+brew cask install jetbrains-toolbox
+brew cask install kaleidoscope
+brew cask install kubernetic
+brew cask install logitech-options
+brew cask install macdown
+brew cask install minikube
+brew cask install moom
+brew cask install opera
+brew cask install paw
+brew cask install proxyman
+brew cask install sequel-pro
+brew cask install skype
+brew cask install slack
+brew cask install sourcetree
+brew cask install spotify
+brew cask install spotmenu
+brew cask install the-unarchiver
+brew cask install trailer
+brew cask install tunnelblick
+brew cask install vagrant
+brew cask install vagrant-manager
+brew cask install virtualbox
+brew cask install visual-studio-code
+brew cask install vlc
