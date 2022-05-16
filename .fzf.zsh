7d6df84820bc68964b10f44692fd51f5c2a35722
Dominik Siebel <code@dsiebel.de>
2022-05-16 10:13:01 +0200

    use HOMEBREW_PREFIX in path
    path has changed on ARM homebrew

--- .fzf.zsh
+++ .fzf.zsh
@@ -6,11 +6,11 @@ fi
 
 # Auto-completion
 # ---------------
-# [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+# [[ $- == *i* ]] && source "${HOMEBREW_PREFIX}/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+source "${HOMEBREW_PREFIX}/opt/fzf/shell/key-bindings.zsh"
 
 # Default options
 export FZF_DEFAULT_OPTS='--height 40% --border --info=inline'
